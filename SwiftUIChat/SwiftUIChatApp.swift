7a365c4c37a9e358d521e354571f3885204b4d96
arturpilavetz <arturpiliavets@gmail.com>
2025-08-04 22:35:03 +0200

    Initial Commit

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Artur Pilavetz on 04.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
